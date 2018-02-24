ab6c7256362c2db9697c99c28bea784c3b683274
Johnny Olson <john.olson3@gmail.com>
2018-02-24 02:24:23 +0000

    remove unneeded variables

--- js/index.js
+++ js/index.js
@@ -2,30 +2,28 @@
 
 var shrinkHeader = (function() {
 
-	var $docElem = $(document.documentElement),
-		  $navbar = $('.navbar'),
-		  $logo = $('.logo-wrapper'),
-		  $navLinks = $('.nav-links ul'),
-		  shrinkPosition = 83,
-		  headerIsShrunk = false,
-		  isChecking = false,
-      yPosition;
+	const shrinkPosition = 83, timeout = 25;
+	var   $docElem = $(document.documentElement),
+		    $navbar = $('.navbar'),
+		    $logo = $('.logo-wrapper'),
+		    $navLinks = $('.nav-links ul'),
+		    isChecking = false;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
 		  if(!isChecking) {
 		    isChecking = true
-		    setTimeout(checkPosition, 25);
+		    setTimeout(checkPosition, timeout);
 		  }
 		}, false );
 	}
 
-	function getYPosition() {
-		return $docElem.scrollTop();
-	}
-	
 	function checkPosition() {
 	  checkForChange(getYPosition());
+	}
+	
+	function getYPosition() {
+		return $docElem.scrollTop();
 	}
 	
 	function checkForChange(position) {
@@ -37,15 +35,13 @@ var shrinkHeader = (function() {
 	function shrinkNavbar () {
 	  $navbar.addClass('navbar-shrink');
 	  $logo.addClass('logo-wrapper-shrink');
-	  $navLinks.css( { 'line-height': '90px'} )
-	  headerIsShrunk = true;
+	  $navLinks.css( { 'line-height': '90px'} );
 	}
 	
 	function expandNavbar () {
 	  $navbar.removeClass('navbar-shrink');
 	  $logo.removeClass('logo-wrapper-shrink');
-    $navLinks.css( { 'line-height': '116px'} )
-	  headerIsShrunk = false;
+    $navLinks.css( { 'line-height': '116px'} );
 	}
 
 	init();
