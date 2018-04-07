1f48f0d1feaaecaea90b806c5ba8d8b7932cd15b
Johnny Olson <john.olson3@gmail.com>
2018-04-07 20:42:26 +0000

    express framework setup and working

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+    app = express();
+
+app.get("/", function(req, res) {
+  res.sendFile('index.html', {root: __dirname});
+});
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log("Server has started...");
+});
